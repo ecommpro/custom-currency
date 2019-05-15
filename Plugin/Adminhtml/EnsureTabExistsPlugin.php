5176526732abb294226f55e12bd196c086b3e347
@manelio <manel.domenech@gmail.com>
2019-05-15 02:00:55 +0200

    Fix for the absence of parent tab

---
+++ Plugin/Adminhtml/EnsureTabExistsPlugin.php
@@ -0,0 +1,26 @@
+<?php
+namespace EcommPro\CustomCurrency\Plugin\Adminhtml;
+
+use Magento\Framework\Config\Data;
+
+class EnsureTabExistsPlugin
+{
+    public function afterGet(\Magento\Config\Model\Config\Structure\Data $subject, $result)
+    {
+        if (!isset($result['tabs']['ecommpro'])) {
+            return array_merge_recursive($result, [
+                'tabs' => [
+                    'ecommpro' => [
+                        'id' => 'ecommpro',
+                        'translate' => 'label',
+                        'sortOrder' => 10,
+                        'label' => 'EcommPro',
+                        '_elementType' => 'tab',
+                    ]
+                ]
+            ]);
+        }
+
+        return $result;
+    }
+}
